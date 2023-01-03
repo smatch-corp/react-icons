e76e6fdd8ea45ca54a438dac53cf1d2230393715
iamchanii <iamchanii@users.noreply.github.com>
2023-01-03 05:48:18 +0000

    feat: import icons

---
+++ src/IconKeyboardArrowLeftRoundedSmall.tsx
@@ -0,0 +1,6 @@
+import { forwardRef } from 'react';
+import type { SVGAttributes } from 'react';
+export const IconKeyboardArrowLeftRoundedSmall = forwardRef<SVGSVGElement, SVGAttributes<SVGSVGElement>>((props, ref) => {
+  return <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}><path d="M13.7815 9.10562L10.8826 12.0045L13.7815 14.9033C14.0728 15.1947 14.0728 15.6654 13.7815 15.9568C13.4901 16.2481 13.0194 16.2481 12.728 15.9568L9.29872 12.5275C9.00734 12.2361 9.00734 11.7654 9.29872 11.474L12.728 8.04471C13.0194 7.75333 13.4901 7.75333 13.7815 8.04471C14.0654 8.33608 14.0728 8.81424 13.7815 9.10562Z" fill="currentColor" /></svg>;
+});
+IconKeyboardArrowLeftRoundedSmall.displayName = "IconKeyboardArrowLeftRoundedSmall";
---
+++ src/IconLogoutRounded.tsx
@@ -0,0 +1,6 @@
+import { forwardRef } from 'react';
+import type { SVGAttributes } from 'react';
+export const IconLogoutRounded = forwardRef<SVGSVGElement, SVGAttributes<SVGSVGElement>>((props, ref) => {
+  return <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}><path d="M5 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H11C11.55 21 12 20.55 12 20C12 19.45 11.55 19 11 19H5V5Z" fill="currentColor" /><path d="M20.65 11.6495L17.86 8.85954C17.54 8.53954 17 8.75954 17 9.20954V10.9995H10C9.45 10.9995 9 11.4495 9 11.9995C9 12.5495 9.45 12.9995 10 12.9995H17V14.7895C17 15.2395 17.54 15.4595 17.85 15.1395L20.64 12.3495C20.84 12.1595 20.84 11.8395 20.65 11.6495Z" fill="currentColor" /></svg>;
+});
+IconLogoutRounded.displayName = "IconLogoutRounded";
