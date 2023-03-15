f1709af5263c15b32ff92b3771e687678a29b7c7
iamchanii <iamchanii.0310@gmail.com>
2023-03-15 09:53:33 +0900

    fix: convert stroke color

--- src/IconSubwayLineDot.tsx
+++ src/IconSubwayLineDot.tsx
@@ -1,6 +1,6 @@
 import { forwardRef } from 'react';
 import type { SVGAttributes } from 'react';
 export const IconSubwayLineDot = forwardRef<SVGSVGElement, SVGAttributes<SVGSVGElement>>((props, ref) => {
-  return <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}><circle cx={12} cy={12} r={4} fill="currentColor" /><circle cx={12} cy={12} r={3.5} stroke="black" strokeOpacity={0.08} /></svg>;
+  return <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}><circle cx={12} cy={12} r={4} fill="currentColor" /><circle cx={12} cy={12} r={3.5} stroke="currentColor" strokeOpacity={0.08} /></svg>;
 });
 IconSubwayLineDot.displayName = "IconSubwayLineDot";
