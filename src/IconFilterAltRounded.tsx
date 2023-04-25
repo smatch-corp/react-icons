27d5892d01b98c5648f29d052b1ea6e9047a6cb6
iamchanii <iamchanii@users.noreply.github.com>
2023-04-25 01:37:41 +0000

    feat: import icons

---
+++ src/IconFilterAltRounded.tsx
@@ -0,0 +1,6 @@
+import { forwardRef } from 'react';
+import type { SVGAttributes } from 'react';
+export const IconFilterAltRounded = forwardRef<SVGSVGElement, SVGAttributes<SVGSVGElement>>((props, ref) => {
+  return <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}><path d="M4.24999 5.61C6.56999 8.59 9.99999 13 9.99999 13V18C9.99999 19.1 10.9 20 12 20C13.1 20 14 19.1 14 18V13C14 13 17.43 8.59 19.75 5.61C20.26 4.95 19.79 4 18.95 4H5.03999C4.20999 4 3.73999 4.95 4.24999 5.61Z" fill="currentColor" /></svg>;
+});
+IconFilterAltRounded.displayName = "IconFilterAltRounded";
