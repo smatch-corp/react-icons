c425d5725346265b2652e25e4b928948c45f238c
iamchanii <iamchanii@users.noreply.github.com>
2023-05-11 06:20:49 +0000

    feat: import icons

---
+++ src/IconInfoRounded.tsx
@@ -0,0 +1,6 @@
+import { forwardRef } from 'react';
+import type { SVGAttributes } from 'react';
+export const IconInfoRounded = forwardRef<SVGSVGElement, SVGAttributes<SVGSVGElement>>((props, ref) => {
+  return <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}><path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 17C11.45 17 11 16.55 11 16V12C11 11.45 11.45 11 12 11C12.55 11 13 11.45 13 12V16C13 16.55 12.55 17 12 17ZM13 9H11V7H13V9Z" fill="currentColor" /></svg>;
+});
+IconInfoRounded.displayName = "IconInfoRounded";
