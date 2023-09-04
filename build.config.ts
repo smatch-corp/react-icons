fefe49904f8c2105c4ff4484578ae829c35e336a
iamchanii <iamchanii.0310@gmail.com>
2023-09-04 13:50:14 +0900

    ci: fix jsx transform

--- build.config.ts
+++ build.config.ts
@@ -3,14 +3,21 @@ import { defineBuildConfig } from 'unbuild';
 export default defineBuildConfig({
     declaration: true,
     entries: [
-        './src/index.ts',
         {
             builder: 'mkdist',
+            esbuild: { jsx: 'automatic' },
+            ext: 'js',
+            format: 'cjs',
+            input: './src',
+            outDir: './dist',
+        },
+        {
+            builder: 'mkdist',
+            esbuild: { jsx: 'automatic' },
+            ext: 'mjs',
+            format: 'esm',
             input: './src',
             outDir: './dist',
         },
     ],
-    rollup: {
-        emitCJS: true,
-    },
 });
