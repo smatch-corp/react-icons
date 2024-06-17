e69566fd4fdbccdf7e651f09911f9b6d7c5d8ba5
pebblepark <pebblepark@users.noreply.github.com>
2024-06-17 08:11:21 +0000

    feat: import icons

---
+++ src/IconGeneralOffice.tsx
@@ -0,0 +1,5 @@
+import type { SVGProps } from "react";
+import { Ref, forwardRef } from "react";
+const IconGeneralOffice = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}><path d="M19.39 8.53H16.16V4.4C16.16 3.63 15.53 3 14.76 3H5.4C4.63 3 4 3.63 4 4.4V20.6C4 21.37 4.63 22 5.4 22H19.39C20.16 22 20.79 21.37 20.79 20.6V9.93C20.79 9.16 20.16 8.53 19.39 8.53ZM8.86 12.84H6.21V10.19H8.86V12.84ZM8.86 7.86H6.21V5.21H8.86V7.86ZM11.18 5.21H13.83V7.86H11.18V5.21Z" fill="currentColor" /></svg>;
+const ForwardRef = forwardRef(IconGeneralOffice);
+export { ForwardRef as IconGeneralOffice };
